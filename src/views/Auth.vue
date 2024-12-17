05a2e3ed0210366823db9d5859e3cfaea6cad11f
WarGhost132 <wawghostick@gmail.com>
2024-12-17 14:18:45 +0300

    Add vee-validate, yup. Add auth form and validation

--- src/views/Auth.vue
+++ src/views/Auth.vue
@@ -1,10 +1,36 @@
 <template>
-  
+  <form class="card" @submit.prevent="onSubmit">
+    <h1>Войти в систему</h1>
+
+    <div :class="['form-control', { invalid: emailError }]">
+      <label for="email">Email</label>
+      <input type="email" id="email" v-model="email" @blur="emailBlur">
+      <small v-if="emailError">{{ emailError }}</small>
+    </div>
+
+    <div :class="['form-control', { invalid: passwordError }]">
+      <label for="password">Password</label>
+      <input type="password" id="password" v-model="password" @blur="passwordBlur">
+      <small v-if="passwordError">{{ passwordError }}</small>
+    </div>
+
+    <button class="btn primary" type="submit" :disabled="isSubmitting || isTooManyAttempts">
+      Войти
+    </button>
+
+    <div class="text-danger" v-if="isTooManyAttempts">
+      Вы слишком часто пытаетесь войти в систему. Попробуйте позже
+    </div>
+  </form>
 </template>
 
 <script>
-export default {
+import { useLoginForm } from '../use/login-form';
 
+export default {
+  setup() {
+    return {...useLoginForm()}
+  }
 }
 </script>
 
