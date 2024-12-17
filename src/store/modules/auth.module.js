0729987789b40015e9e4c79139217164224eb500
WarGhost132 <wawghostick@gmail.com>
2024-12-17 18:01:51 +0300

    fix routing with error

--- src/store/modules/auth.module.js
+++ src/store/modules/auth.module.js
@@ -33,6 +33,7 @@ export default {
         commit("setToken", data.idToken);
       } catch (e) {
         console.log(error(e.response.data.error.message));
+        throw new Error();
       }
     },
   },
