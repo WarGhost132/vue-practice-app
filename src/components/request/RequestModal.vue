f6774b0519b122cffeca7ef2fce008b804a9c30f
WarGhost132 <wawghostick@gmail.com>
2024-12-18 15:23:54 +0300

    add form for modal

--- src/components/request/RequestModal.vue
+++ src/components/request/RequestModal.vue
@@ -1,15 +1,52 @@
 <template>
-  <form>
-    <div class="form-control">
-      <label for="">ФИО</label>
-      <input type="text">
+  <form @submit.prevent="onSubmit">
+    <div class="form-control" :class="{invalid: fioError}">
+      <label for="fio">ФИО</label>
+      <input type="text" id="fio" v-model="fio" @blur="fioBlur">
+      <small v-if="fioError">{{ fioError }}</small>
     </div>
+
+    <div class="form-control" :class="{invalid: phoneError}">
+      <label for="phone">Телефон</label>
+      <input type="text" id="phone" v-model="phone" @blur="phoneBlur">
+      <small v-if="phoneError">{{ phoneError }}</small>
+    </div>
+
+    <div class="form-control" :class="{invalid: amountError}">
+      <label for="amount">Суммы</label>
+      <input type="number" id="amount" v-model.number="amount" @blur="amountBlur">
+      <small v-if="amountError">{{ amountError }}</small>
+    </div>
+
+    <div class="form-control">
+      <label for="status">Статус</label>
+      <select id="status" v-model="status">
+        <option value="done">Завершен</option>
+        <option value="cancelled">Отменен</option>
+        <option value="active">Активен</option>
+        <option value="pending">Выполняется</option>
+      </select>
+    </div>
+
+    <button class="btn primary" :disabled="isSubmitting">Создать</button>
   </form>
 </template>
 
 <script>
-export default {
+import { useRequestForm } from '../../use/request-form'
 
+export default {
+  emits: ['created'],
+  setup(_, { emit }) {
+    const submit = async values => {
+      console.log(values)
+      emit('created')
+    }
+
+    return {
+      ...useRequestForm(submit)
+    }
+  }
 }
 </script>
 
